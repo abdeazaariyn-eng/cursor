3eceed587272ba2bd61d9fe9b3b06b9c7a8a21f8
Smiya Dialak <limail_dialak@gmail.com>
2026-08-02 04:21:26 +0100

    Fix: Remove invalid quality option from next.config.js images (should only be in Image component)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
     minimumCacheTTL: 604800,
     deviceSizes: [360, 414, 640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    quality: 75,
     remotePatterns: [],
   },
 
